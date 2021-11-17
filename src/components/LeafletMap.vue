5cc692e0383d883f14e56703ca36108f0fa042c0
Liina Jaanus <MM-20-377@tptlive.ee>
2021-11-17 11:45:55 +0200

    lol

---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+export default {
+    props: ['lat', 'lng', 'zoom', 'geoJson'],
+    mounted(){
+        this.map = L.map('map').setView([this.lat, this.lng], this.zoom);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1IjoibGlpbmFqYWFuIiwiYSI6ImNrdzM3bWtuYWUxdWkyb3M3dmp1d3g5bzEifQ.zLq6Uf4w4zWyaa2kugQbew"
+        }).addTo(this.map);
+            L.geoJson(this.geoJson, {style: this.style}).addTo(this.map);
+    },
+    data(){
+        return{
+            map:null
+        }
+    },
+    methods:{
+        getColors(d){
+        return d > 1000000 ? '#800026' :
+                d > 100000  ? '#BD0026' :
+                d > 10000  ? '#E31A1C' :
+                d > 1000  ? '#FC4E2A' :
+                d > 100   ? '#FD8D3C' :
+                d > 10  ? '#FEB24C' :
+                d == -1   ? '#000' :
+                            '#FFEDA0';
+        },
+        style(feature) {
+    return {
+        fillColor: this.getColors(feature.properties.confirmed),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+    },
+    watch: {
+        lat(lat){
+            this.map.flyTo([lat, this.lng], this.zoom);
+        },
+        lng(lng){
+            this.map.flyTo([this.lat, lng], this.zoom);
+        },
+        zoom(zoom){
+            this.map.flyTo([this.lat, this.lng], zoom);
+        },
+        geoJson(geoJson){
+            L.geoJson(this.geoJson, {style: this.style}).addTo(this.map);
+        }
+    }
+}
+</script>
+
+<style>
+    #map { 
+        height: 800px; }
+</style>
--- src/components/MapExample.vue
+++ src/components/MapExample.vue
@@ -1,30 +1,65 @@
 <template>
 <div>
     <button class="button is-primary" @click="goToHome">Go to home</button>
-    <google-maps :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></google-maps>
+    <!-- <google-maps :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></google-maps> -->
+    <leaflet-map :lat="lat" :lng="lng" :zoom="zoom" :geoJson="covidGeoJson"></leaflet-map>
 </div>
 </template>
 
 <script>
-import GoogleMaps from './GoogleMaps.vue'
+import GoogleMaps from './GoogleMaps.vue';
+import LeafletMap from './LeafletMap.vue';
+import axios from 'axios';
 export default {
-  components: { GoogleMaps },
+  components: { GoogleMaps, LeafletMap },
+    created(){
+        let url = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+        axios.get(url).then(response =>{
+            console.log(response.data);
+            this.geoJson = response.data;
+        });
+        axios.get('https://api.covid19api.com/summary').then(response => {
+            console.log(response);
+            this.countries=response.data.Countries;
+        })
+    },
     data(){
         return {
             lat: 59.4265182,
             lng: 24.7431717,
-            zoom: 18.25
+            zoom: 18.25,
+            geoJson: null,
+            countries: null
         }
     },
     methods: {
         goToHome(){
-            this.lat=59.32471411,
-            this.lng=24.5557187,
-            this.zoom=18
+            this.lat=59.4113121;
+            this.lng=24.7031906;
+            this.zoom=18;
+        }
+    },
+    computed: {
+        covidGeoJson(){
+            if(this.geoJson && this.countries){
+                let covidGeoJson = {...this.geoJson};
+                covidGeoJson.features = this.geoJson.features.map(feature => {
+                    let country = this.countries.find(country => country.Country == feature.properties.name);
+                    feature.properties.confirmed=-1;
+                    feature.properties.deaths=-1;
+            if(country){
+                feature.properties.confirmed = country.TotalConfirmed;
+                feature.properties.deaths = country.TotalDeaths;
+            }
+                return feature;
+            });
+            return covidGeoJson;
+            }
+            return null;
         }
     }
 }
 </script>
 
 <style>
-</style> 
+</style>
